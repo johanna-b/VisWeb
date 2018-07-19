76cc68e82361f433ad4e88173b569865c65854aa
Sushant Achawal <sushachawal@gmail.com>
2018-07-19 15:41:04 -0400

    Add support to initialise the server from the JavaScript client

--- app/js/websocket_client.js
+++ app/js/websocket_client.js
@@ -10,7 +10,13 @@ function WebSocketTest() {
         ws.onopen = function() {
 
             // Web Socket is connected, send data using send()
-            ws.send("Message to send");
+            var HVR_Message = {
+                MessageId: HVRMessageId.HVR_INIT_REQUEST,
+                numArgs: 2,
+                options: [" ", "dll", "--app_petascale"],
+            };
+
+            ws.send(JSON.stringify(HVR_Message));
             alert("Message is sent...");
         };
 
@@ -31,8 +37,45 @@ function WebSocketTest() {
     }
 }
 
-function GenerateMessage() {
-    var arguments = {0:"Hello", 1:"world"};
 
-    var buff = new ArrayBuffer();
-}
+const HVRMessageId = {
+
+    INVALID: 0,
+    EXIT: 1,
+
+    HVR_SERVER_LAUNCHED: 2,
+    CLOSE_VOLUME: 3,
+    HVR_FILE_LIST: 4,
+    HVR_LAUNCH_SERVER: 5,
+    HVR_INIT_REQUEST: 6,
+    HVR_INIT_PARALLEL: 7,
+    HVR_VOLUME_LIST_REQUEST: 8,
+    HVR_LOAD_FILE: 9,
+    HVR_LOAD_DATA: 10,
+
+    HVR_VIEWSETTINGS: 11,
+    HVR_CAMERA: 12,
+    HVR_OBJECTSETTINGS: 13,
+    HVR_TF: 14,
+    HVR_KEY: 15,
+    HVR_FRAME_UPDATE: 16,
+
+    HVR_IMAGE: 17,
+
+    HVR_THIN_CLIENT: 18,
+    HVR_INIT_DATA: 19,
+    HVR_META_DATA: 20,
+    HVR_CLUSTER_SETTINGS: 21,
+    HVR_OCCLUSION_INFO: 22,
+
+    HVR_RENDERED_TILES: 23,
+    HVR_COMPOSITED_TILES: 24,
+
+    HVR_FRAME_RENDERING_COMPLETE: 25,
+    HVR_FRAME_COMPOSITING_COMPLETE: 26,
+
+    HVR_STATE: 27,
+    HVR_NB_INIT: 28,
+    HVR_VASTIDS: 29,
+    HVR_VASTIDS_COMPARISON: 30
+};
