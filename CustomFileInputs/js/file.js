70e3640b2a7435a6b0de30cf44b7db7e59bdd8aa
zevnicsca <zevnicsca@gmail.com>
2019-06-20 12:11:24 -0400

    file

--- CustomFileInputs/js/file.js
+++ CustomFileInputs/js/file.js
@@ -9,28 +9,30 @@ function handleFile(files) {
     var reader = new FileReader();
     var label = document.getElementById("label");
     var input = document.getElementById("input");
-    label.innerHTML = f.name;
+    label.innerHTML = "Loading ..."
+    
 
     // Closure to capture the file information.
     reader.onloadstart = function function_name(argument) {
-      input.style.background = "linear-gradient(90deg, #722040 50%, #d3394c 50%);"
-      console.log(input)
+      input.style.background = "linear-gradient(90deg, #722040 0%, #d3394c 0%)"
     }
 
     reader.onprogress = function(data) {
       if (data.lengthComputable) {                                            
           var progress = parseInt( ((data.loaded / data.total) * 100), 10 );
-          console.log(progress);
-          // bar TODO
+          input.style.background = "linear-gradient(90deg, #722040 " + progress + "%, #d3394c " + progress + "%)"
       }
     }
 
     reader.onerror = function(evt) {
-      // todo
+      label.innerHTML = "Error"
+      input.style.background = "initial"
     };
 
     reader.onload = function(data){
       console.log(new Uint8Array(data.target.result))
+      input.style.background = "linear-gradient(90deg, #722040 100%, #d3394c 100%)"
+      label.innerHTML = f.name;
     }
 
     reader.readAsArrayBuffer(f);
@@ -54,8 +56,18 @@ function handleFile(files) {
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
   };
 
+  function handelDragEnter(evt) {
+  	document.getElementById("drop_zone").style.outlineColor = "#d3394c";
+  }
+
+  function handelDragLeave(evt) {
+  	document.getElementById("drop_zone").style.outlineColor = "#f1e5e6";
+  }
+
   var inn = document.getElementById('file-1');
   inn.addEventListener('change', handleFileSelect, false);
   var drop = document.getElementById('drop_zone');
   drop.addEventListener('dragover', handleDragOver, false);
   drop.addEventListener('drop', handleFileDrop, false);
+  drop.addEventListener('dragenter', handelDragEnter, false);
+  drop.addEventListener('dragleave', handelDragLeave, false);
